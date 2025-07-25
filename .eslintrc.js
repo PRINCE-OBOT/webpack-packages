be5662708926dc08cf7e8854e494d2214684a179
PRINCE-OBOT <princessamuel45@gmail.com>
2025-07-25 22:24:09 +0100

    Add more packages

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "airbnb-base",
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    // You can override Airbnb rules here if you want
+  },
+};
